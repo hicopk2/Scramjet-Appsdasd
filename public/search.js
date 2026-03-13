35862f595c602c6ff9b7b1097c2581800ce074a5
hicopk/the best <hicopkv2@gmail.com>
2026-03-13 15:40:20 -0400

    Refactor search function and add event listener

--- public/search.js
+++ public/search.js
@@ -1,32 +1,62 @@
 "use strict";
 /**
- *
- * @param {string} input
- * @param {string} template Template for a search query.
- * @returns {string} Fully qualified URL
+ * @type {HTMLFormElement}
  */
-function search(input, template) {
-	try {
-		// input is a valid URL:
-		// eg: https://example.com, https://example.com/test?q=param
-		return new URL(input).toString();
-	} catch (err) {
-		// input was not a valid URL
-	}
+const form = document.getElementById("sj-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("sj-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("sj-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("sj-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("sj-error-code");
+
+const { ScramjetController } = $scramjetLoadController();
+
+const scramjet = new ScramjetController({
+	files: {
+		wasm: '/scram/scramjet.wasm.wasm',
+		all: '/scram/scramjet.all.js',
+		sync: '/scram/scramjet.sync.js',
+	},
+});
+
+scramjet.init();
+
+const connection = new BareMux.BareMuxConnection("/baremux/worker.js");
+
+form.addEventListener("submit", async (event) => {
+	event.preventDefault();
 
 	try {
-		// input is a valid URL when http:// is added to the start:
-		// eg: example.com, https://example.com/test?q=param
-		const url = new URL(`http://${input}`);
-		// only if the hostname has a TLD/subdomain
-		if (url.hostname.includes(".")) return url.toString();
+		await registerSW();
 	} catch (err) {
-		// input was not valid URL
+		error.textContent = "Failed to register service worker.";
+		errorCode.textContent = err.toString();
+		throw err;
 	}
 
-	// input may have been a valid URL, however the hostname was invalid
+	const url = search(address.value, searchEngine.value);
 
-	// Attempts to convert the input to a fully qualified URL have failed
-	// Treat the input as a search query
-	return template.replace("%s", encodeURIComponent(input));
-}
+	let wispUrl =
+		(location.protocol === "https:" ? "wss" : "ws") +
+		"://" +
+		location.host +
+		"/wisp/";
+	if ((await connection.getTransport()) !== "/epoxy/index.mjs") {
+		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+	}
+  const frame = scramjet.createFrame();
+  frame.frame.id = "sj-frame";
+	document.body.appendChild(frame.frame);	
+	frame.go(url);
+});
